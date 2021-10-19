22f0aebdabf71c0d6c161b77bcf213e7c011febe
Mateen Mirani <mateenmirani125@gmail.com>
2021-10-19 15:04:30 +0500

    Create Meal-summary component

---
+++ src/components/Meals/Meal-Summary/MealSummary.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import classes from "./MealSummary.module.css";
+
+const MealSummary = () => {
+  return (
+    <div className={classes.summary}>
+      <h2>Delicious Food, Delivered to you!</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        necessitatibus cupiditate illo atque aperiam temporibus!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        necessitatibus cupiditate illo atque aperiam temporibus!
+      </p>
+    </div>
+  );
+};
+
+export default MealSummary;
---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MealSummary from "./Meal-Summary/MealSummary";
+
+const Meals = () => {
+  return (
+    <>
+      <MealSummary />
+    </>
+  );
+};
+
+export default Meals;
