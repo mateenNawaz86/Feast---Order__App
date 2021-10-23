ae7b3ad1f5274907d55311a6598aca624359c189
Mateen Mirani <mateenmirani125@gmail.com>
2021-10-23 10:58:49 +0500

    setting up context API

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Cart from "./components/Cart/Cart";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./context/CartProvider";
 
 const App = () => {
   const [showCart, setShowCart] = useState(false);
@@ -11,13 +12,13 @@ const App = () => {
   const hideCartHandler = () => setShowCart(!showCart);
 
   return (
-    <>
+    <CartProvider>
       <Header onShowCart={showCartHandler} />
       {showCart && <Cart onHideCart={hideCartHandler} />}
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 };
 
--- src/components/Meals/Meal-Item/MealItem.jsx
+++ src/components/Meals/Meal-Item/MealItem.jsx
@@ -9,7 +9,7 @@ const MealItem = (props) => {
     <div className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
-        <div className={classes.description}>{classes.description}</div>
+        <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
       <MealForm />
